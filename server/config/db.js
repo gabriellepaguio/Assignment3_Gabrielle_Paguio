6fd54b2ffaf70c59727720edc0aff9d99ff8472b
gabriellepaguio <113075617+gabriellepaguio@users.noreply.github.com>
2022-11-13 17:29:22 -0500

    changed URI from localhost to mongodb cloud

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@ module.exports =
 {
     // provides the database of assignment tracker from mongosh
     // downloaded mongoose library to access the database - to connect
-    "URI": "mongodb://localhost/assignmentTracker"
+    // URI link is connected to the cloud for MongoDB not local host
+    "URI": "mongodb+srv://gabriellepaguio:as3CRUD@cluster0.tgcatwg.mongodb.net/Assignment_Tracker"
 }
